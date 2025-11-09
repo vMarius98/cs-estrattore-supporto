19764afeb579ba95f479e506f81c0d36c1728464
vMarius <165833101+vMarius98@users.noreply.github.com>
2025-11-09 23:36:35 +0100

    Remove mobile disable option from AOS config
    Removed the 'disable' option for mobile in AOS initialization.

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ AOS.init({
     duration: 1000,
     once: true,
     offset: 100,
-    easing: 'ease-out-cubic',
-    disable: 'mobile'
+    easing: 'ease-out-cubic'
+    // La riga "disable: 'mobile'" è stata rimossa!
 });
 
 // ============================================
